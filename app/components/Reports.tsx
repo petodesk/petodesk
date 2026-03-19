b0901c11f3029d415616fe348bd48643873a0367
sudais73 <sudaisman193@gmail.com>
2026-03-19 15:30:25 +0300

    reports page updated

--- app/components/Reports.tsx
+++ app/components/Reports.tsx
@@ -24,6 +24,7 @@ export default function Reports({ onClose, open }: { onClose: () => void, open: 
 
     useEffect(() => {
         fetchTasks()
+        fetchUser()
 
         const channel = supabase
             .channel('realtime-reports')
