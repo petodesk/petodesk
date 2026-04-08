943447424e769f91735b794d08b8c3d5ac0846b1
sudais73 <sudaisman193@gmail.com>
2026-04-08 10:54:52 +0300

    invite page updated

--- app/actions/inviteTeams.ts
+++ app/actions/inviteTeams.ts
@@ -6,6 +6,7 @@ import { createClient } from '@supabase/supabase-js'
 export async function inviteTeams(
   email: string,
   role: string,
+  name: string,
   company: any
 ) {
 
@@ -54,8 +55,11 @@ export async function inviteTeams(
         to: [email],
         subject: `You're now an Admin at ${company.name}`,
         html: `
-          <h2>Congratulations </h2>
-          <p>You have been promoted to <strong>ADMIN</strong> at <strong>${company.name}</strong>.</p>
+          <h2>Hello ${name}  </h2>
+          <p>
+          You have been assigned as an ADMIN at ${company.name}.  
+
+           You have been promoted to <strong>ADMIN</strong> at <strong>${company.name}</strong>.</p>
           
           <p>You now have access to admin features and controls.</p>
 
