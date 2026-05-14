77e856726d2dc544643d19753928e93207c31211
sudais73 <sudaisman193@gmail.com>
2026-05-14 23:08:30 +0300

    simple check

--- app/components/dashboards/inventoryPlanDash.tsx
+++ app/components/dashboards/inventoryPlanDash.tsx
@@ -274,7 +274,7 @@ const profitTrend = netProfit - previousMonthProfit
 
     <div className="p-6 rounded-lg font-poppins bg-gray-50 max-h-[90vh] overflow-y-auto no-scrollbar">
 
-      <div className="flex flex-col gap-6">
+      <div className="flex flex-col gap-7">
 
         {/* FILTER */}
 
